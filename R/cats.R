d487fdc48e62260e6f8a9ce1f92ef9510d10050b
Beka Stiling <stilir@uw.edu>
2021-02-24 11:23:18 -0800

    Cat package and documentation added.

--- R/cats.R
+++ R/cats.R
@@ -1,3 +1,11 @@
+#' Expresses your feelings about pets
+#'
+#' @param love A logical argument indicating whether or not you like cats
+#'
+#' @return One of two character messages about cat feelings e.g. `I love cats`
+#' @export
+#'
+#' @examples cats(TRUE)
 cats <- function(love = TRUE) {
   if(love == TRUE) {
     msg <- "I love cats!"
